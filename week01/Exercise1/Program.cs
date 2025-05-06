81da49dabc9900de47ae6a0eea56170fdb64076c
Oducity <henry.odumamwen@gmail.com>
2025-05-06 13:09:23 +0100

    Added comments

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,12 +5,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Asked for the user's first name.
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
-
+        // Asked for user's last name.
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
-        
+        // Print user's last name, full name to the terminal.
         Console.WriteLine($"{lastName}, {firstName} {lastName}");
     }
 }
