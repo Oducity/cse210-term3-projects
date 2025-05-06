12d2b32c38884db9d673ca9430a4e6b3696307d8
Oducity <henry.odumamwen@gmail.com>
2025-05-06 14:29:06 +0100

    Did individual if else if else console.WriteLine

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percent: ");
+        string number = Console.ReadLine();
+        float score = float.Parse(number);
+
+        if (score >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (score >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (score >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (score >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("E");
+        }
     }
 }
