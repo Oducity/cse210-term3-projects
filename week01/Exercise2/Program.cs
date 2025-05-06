53d93b06f7550b1e74703115495b90e72e959944
Oducity <henry.odumamwen@gmail.com>
2025-05-06 14:52:47 +0100

    Added congratulatory messages.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -8,26 +8,34 @@ class Program
         Console.Write("Enter your grade percent: ");
         string number = Console.ReadLine();
         float score = float.Parse(number);
-
-        if (score >= 90)
+        if (score >= 70)
         {
-            Console.WriteLine("A");
-        }
-        else if (score >= 80)
-        {
-            Console.WriteLine("B");
-        }
-        else if (score >= 70)
-        {
-            Console.WriteLine("C");
-        }
-        else if (score >= 60)
-        {
-            Console.WriteLine("D");
+            if (score >= 90)
+            {
+                Console.WriteLine("Grade: A");
+            }
+            else if (score >= 80)
+            {
+                Console.WriteLine("Grade: B");
+            }
+            else if (score >= 70)
+            {
+                Console.WriteLine("Grade: C");
+            }
+            Console.WriteLine("Congratulations! You made it.");
         }
         else
         {
-            Console.WriteLine("E");
+            if (score >= 60)
+            {
+                Console.WriteLine("Grade: D");
+            }
+            else
+            {
+                Console.WriteLine("Grade: E");
+            }
+            Console.WriteLine("Haven't come this far, with little more effort, You're going to make it. Keep trying.");
         }
+
     }
 }
