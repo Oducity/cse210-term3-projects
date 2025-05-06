1e1653d96fafab1b09c9505c284204ebd3db89d7
Oducity <henry.odumamwen@gmail.com>
2025-05-06 21:15:17 +0100

    week01 exercise3 solution.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 100);
+
+        int guess = 0;
+
+        int count = 0; // initialize count to zero.
+
+
+        while (guess != randomNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            count += 1; // Increment count by one every iteration.
+
+            if (guess > randomNumber) //Compare guess to randomNumber.
+            {
+                Console.WriteLine("guess lower!");
+            }
+            else if (guess < randomNumber)
+            {
+                Console.WriteLine("guess higher!");
+            }
+            else
+            {
+                Console.WriteLine("Your guess is correct.");
+            }
+        }
+        Console.WriteLine($"You guessed {count} time(s).");
+
     }
 }
