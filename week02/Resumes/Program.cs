2256ed51f077c0357b3a5fab28403c40b3df0b42
Oducity <henry.odumamwen@gmail.com>
2025-07-04 15:00:32 +0100

    Updated the resume app to request for details from the user.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,20 +5,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        //Console.WriteLine("Hello World! This is the Resumes Project.");
         Job job1 = new Job();  // Created class object and assign attributes values//
-        job1._company = "Microsoft";
-        job1._jobTitle = "Software Engineer";
-        job1._startYear = 2020;
-        job1._endYear = 2023;
+        Console.Write("Enter the first company you worked in: ");
+        job1._company = Console.ReadLine();
+        Console.Write("Enter your job title: ");
+        job1._jobTitle = Console.ReadLine();
+        Console.Write("Enter the year you started working here. ");
+        job1._startYear = double.Parse(Console.ReadLine());
+        Console.Write("Enter the year you stoped working here. ");
+        job1._endYear = double.Parse(Console.ReadLine());
         job1.DisplayDetails();  // Called the DisplayDetails method //
 
 
         Job job2 = new Job();  // Created class object and assign attributes values//
-        job2._company = "Apple";
-        job2._jobTitle = job1._jobTitle;
-        job2._startYear = job1._endYear;
-        job2._endYear = 2025;
+        Console.Write("Enter the first company you worked in: ");
+        job2._company = Console.ReadLine();
+        Console.Write("Enter your job title: ");
+        job2._jobTitle = Console.ReadLine();
+        Console.Write("Enter the year you started working here. ");
+        job2._startYear = double.Parse(Console.ReadLine());
+        Console.Write("Enter the year you stoped working here. ");
+        job2._endYear = double.Parse(Console.ReadLine());
         job2.DisplayDetails();  // Called the DisplayDetails method //
 
         Resume resume = new Resume(); // Create the Resume class Object and assign its member attributes. //
